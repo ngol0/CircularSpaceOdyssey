24b5ce1c97d89ea9fd5f956ae7317613ed8b4118
Lam Ngo <44044782+ngol0@users.noreply.github.com>
2024-01-25 00:04:38 +0330

    more comments

--- GameTest/src/Global/Event.h
+++ GameTest/src/Global/Event.h
@@ -2,16 +2,16 @@
 * It uses function pointer for observers
 */
 #pragma once
-
 #include <map>
 
 template<typename T, typename... TArgs>
 class Event
 {
 private:
-	T* m_instance;
-	void(T::* m_function)(TArgs...);
+	T* m_instance; //instance pointer
+	void(T::* m_function)(TArgs...); //function pointer with TArgs arguments
 
+	//a container for all the observers
 	std::vector<std::pair<T*, void(T::*)(TArgs...)>> m_observers;
 
 
@@ -19,6 +19,8 @@ public:
 	Event() : m_instance(nullptr) {};
 	~Event() {};
 
+	//notify all registered observers
+	//by calling their member function pointer with the provided arguments
 	inline void Notify(const TArgs&...args)
 	{
 		if (m_observers.size() == 0) return;
@@ -29,6 +31,8 @@ public:
 		}
 	}
 
+	//registers an observer by storing its instance pointer and function pointer
+	//in the m_observers vector
 	inline void Register(T* instance, void (T::* function)(TArgs...))
 	{	
 		m_instance = instance;
@@ -39,6 +43,8 @@ public:
 		m_observers.emplace_back(function_pointer);
 	}
 
+	//unregister an observer by removing its instance pointer and function pointer
+	//from the m_observers vector
 	inline void Unregister(T* instance, void (T::* function)(TArgs...))
 	{
 		auto iter = std::remove_if(m_observers.begin(), m_observers.end(),
