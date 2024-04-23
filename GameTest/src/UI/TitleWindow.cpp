0071e6716b635470ac0cfa18a942ac4a76e2b30b
lampngo <lamngo.251@gmail.com>
2024-04-23 22:33:00 +0100

    gif

--- GameTest/src/UI/TitleWindow.cpp
+++ GameTest/src/UI/TitleWindow.cpp
@@ -18,8 +18,8 @@ void TitleWindow::HandleInput(float deltaTime)
 
 void TitleWindow::Render()
 {
-	App::Print(300.f, 600.f, "Welcome to Circular Space Odyssey.", 0.859f, 0.91f, 0.043f, m_FONT);
-	App::Print(300.f, 550.f, "Kill all enemies to collect enough resources & move to next level.", 1.f, 1.f, 1.f, m_FONT);
+	//App::Print(700.f, 600.f, "Welcome.", 1.f, 1.f, 1.f, m_FONT);
+	//App::Print(300.f, 550.f, "Kill all enemies to collect enough resources & move to next level.", 1.f, 1.f, 1.f, m_FONT);
 	App::Print(300.f, 450.f, "A or D to move", 1.f, 1.f, 1.f, m_FONT);
 	App::Print(300.f, 400.f, "LEFT CLICK to shoot", 1.f, 1.f, 1.f, m_FONT);
 	App::Print(300.f, 350.f, "R to restart", 1.f, 1.f, 1.f, m_FONT);
